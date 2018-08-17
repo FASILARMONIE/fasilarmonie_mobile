d6dbea0b5c39cefb23d48426aca41701844739eb
cwallez <catherine.wallez@acseo-conseil.fr>
2018-08-17 10:14:23 +0200

    correction affichage note et frequence

--- src/pages/accord/accord.ts
+++ src/pages/accord/accord.ts
@@ -29,15 +29,19 @@ export class AccordPage {
   public notes: any;
   public index = ['DO', 'DO#', 'RE', 'RE#', 'MI', 'FA', 'FA#', 'SOL', 'SOL#', 'LA', 'LA#', 'SI'];
   public accordage = { index: 9, frequence: 440.0 };
+  public indexLA: number = 9;
+  public frequence : number = 440.0;
   public majeure = [0, 4, 7];
   public Dominante7eme = this.majeure.concat([10]);
-  public Octave = (note: number) => {
+
+  public Octave = (note: any) => {
     let result: any[] = []
     const semitones = Math.round(12 * Math.log(note / 440) * Math.LOG2E)
     // console.log(semitones)
     for (let i = semitones; i < 12 - Math.abs(semitones); i++) {
       //a verifier
-      result.push({ index: this.index[(this.accordage.index + i) % 12], frequence: 2 ** (i / 12) * 440 })
+      let index = (this.accordage.index + i) % 12;
+      result.push({ index: this.index[index], frequence: 2 ** (i / 12) * 440 })
       // result.push(2 ** (j / 12) * 440)
     }
     return result
@@ -50,21 +54,26 @@ export class AccordPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public audioProvider: AudioProvider) {
     this.note = this.navParams.get('note');
+    console.log(this.note.frequence);
     console.log(this.note);
+    
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AccordPage');
-    this.loadAccord();
+   // this.loadAccord();
   }
 
   loadAccord() {
-    let octave = this.Octave(this.note.frequence);
+// let octave = this.Octave(this.note.frequence);
+let octave = this.Octave(this.note);
     this.accord = this.Dominante7eme.map(note => octave[note]);
     this.accord.map(note => {
-      note.oscillator = this.audioProvider.playFrequence(note.frequence);
-      note.playing = true;
+console.log(note);
+
+      // note.oscillator = this.audioProvider.playFrequence(note.frequence);
+      // note.playing = true;
     })
   }
 
--- src/pages/harmony/harmony.ts
+++ src/pages/harmony/harmony.ts
@@ -20,15 +20,19 @@ export class HarmonyPage {
   public notes: any;
   public index = ['DO', 'DO#', 'RE', 'RE#', 'MI', 'FA', 'FA#', 'SOL', 'SOL#', 'LA', 'LA#', 'SI'];
   public accordage = { index: 9, frequence: 440.0 };
+  public indexLA: number = 9;
+  public frequence : number = 440.0;
   public majeure = [0, 4, 7];
   public Dominante7eme = this.majeure.concat([10]);
-  public Octave = (note: number) => {
+
+  public Octave = (note: any) => {
     let result: any[] = []
     const semitones = Math.round(12 * Math.log(note / 440) * Math.LOG2E)
     // console.log(semitones)
     for (let i = semitones; i < 12 - Math.abs(semitones); i++) {
       //a verifier
-      result.push({ index: this.index[(this.accordage.index + i) % 12], frequence: 2 ** (i / 12) * 440 })
+      let index = (this.accordage.index + i) % 12;
+      result.push({ index: this.index[index], frequence: 2 ** (i / 12) * 440 })
       // result.push(2 ** (j / 12) * 440)
     }
     return result
@@ -38,19 +42,23 @@ export class HarmonyPage {
   public note: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public noteProvider: NoteProvider, public audioProvider: AudioProvider) {
-    this.octave = this.Octave(261.6255653005986);
-   
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HarmonyPage');
     this.notes = this.index;
+    console.log(this.notes);
+    
+    this.octave = this.Octave(261.6255653005986)
+    console.log( this.octave);
+    
+    
   }
 
 
   choixNote(note: any) {
     console.log(note);
-    this.audioProvider.stop()
+    // this.audioProvider.stop()
     if (note != null) {
       this.noteSelected = true;
       this.navCtrl.push(AccordPage, {note: note})
